6ac3e33300068465355292984acb9439838e2d9f
merve99celebi <merve99celebi@gmail.com>
2025-10-15 00:26:33 +0300

    203,204,205 are created

--- src/test/java/US_203.java
+++ src/test/java/US_203.java
@@ -10,23 +10,21 @@ import java.time.Duration;
 
 public class US_203 extends BaseDriver {
 
-    US_204 login= new US_204();
-
-    WebDriverWait wait= new WebDriverWait(driver, Duration.ofSeconds(6));
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
 
     @Test
-    public void logOutSuccessfully(){
-        login.loginSuccessfully();
+    public void logOutSuccessfully() {
+        driver.get("https://demowebshop.tricentis.com/");
 
-        WebElement logOutBtn=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@class='ico-logout']")));
+        WebElement logOutBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@class='ico-logout']")));
         wait.until(ExpectedConditions.visibilityOf(logOutBtn));
         logOutBtn.click();
 
-        WebElement verifyLogOut= driver.findElement(By.xpath("//a[@class='ico-login']"));
+        WebElement verifyLogOut = driver.findElement(By.xpath("//a[@class='ico-login']"));
         wait.until(ExpectedConditions.visibilityOf(verifyLogOut));
-        String actualText= verifyLogOut.getText();
+        String actualText = verifyLogOut.getText();
         Assert.assertTrue(actualText.contains("Log in"));
 
-
+        tearDown();
     }
 }
